12697cbf63f5cd4ef5d7cdf65486b3396d7077db
SAKSHAMGARG21 <sakshamgarggarg83@gmail.com>
2025-11-27 15:37:23 +0530

    new file added

---
+++ JavaScript-learning/index2.js
@@ -0,0 +1,13 @@
+function buildArray() {
+  const arr = [];
+  for (var i = 0; i < 3; i++) {
+    arr.push(function() {
+      console.log(i);
+    });
+  }
+  return arr;
+}
+const fns = buildArray();
+fns[0]();
+fns[1]();
+fns[2]();
