8967ca976aacee9eb99118c61a519b95fad381f1
SakshamChicmic21 <saksham.garg@chicmicstudios.in>
2025-12-02 13:25:31 +0530

    alarm clock first commit

--- alarmClock/index.js
+++ alarmClock/index.js
@@ -1,1 +1,12 @@
 console.log("Alarm clock module loaded");
+const alarmTime = document.getElementById("alarmTime");
+const setAlarmBtn = document.getElementById("setAlarmBtn");
+const clearAlarmBtn = document.getElementById("clearAlarmBtn");
+const status = document.getElementById("status");
+
+// alarmTime. = new Date;
+console.log(alarmTime);
+setAlarmBtn.addEventListener("click",()=>{
+    let time = alarmTime.value;
+    console.log(time);
+})
