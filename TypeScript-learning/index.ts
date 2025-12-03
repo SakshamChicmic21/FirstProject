5555b1f59fb454c21924423358ad12fbe55aa7b8
SakshamChicmic21 <saksham.garg@chicmicstudios.in>
2025-12-03 18:42:49 +0530

    check code ts

---
+++ TypeScript-learning/index.ts
@@ -0,0 +1,25 @@
+console.log("ts");
+let message: string = "Hello, TypeScript!";
+console.log(message);
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+class User{
+    name: string;
+    age: number;
+    
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    greet(): void {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+const user : person = new User("Alice", 30);
+user.greet();
