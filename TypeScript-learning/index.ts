c046fcbededf835e7cab0c56284c8ad357a953bd
SAKSHAMGARG21 <sakshamgarggarg83@gmail.com>
2025-12-04 17:26:20 +0530

    Refactor: Comment out unused code and maintain Backpack implementation

--- TypeScript-learning/index.ts
+++ TypeScript-learning/index.ts
@@ -1,26 +1,45 @@
-console.log("ts");
-let message: string = "Hello, TypeScript!";
-console.log(message);
+// console.log("ts");
+// let message: string = "Hello, TypeScript!";
+// console.log(message);
 
-interface Person {
-    name: string;
-    age: number;
-    greet(): void;
-}
+// interface Person {
+//     name: string;
+//     age: number;
+//     greet(): void;
+// }
 
-class User{
-    name: string;
-    age: number;
+// class User{
+//     name: string;
+//     age: number;
     
-    constructor(name: string, age: number){
-        this.name = name;
-        this.age = age;
-    }
+//     constructor(name: string, age: number){
+//         this.name = name;
+//         this.age = age;
+//     }
 
-    greet(): void {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-    }
+//     greet(): void {
+//         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//     }
+// }
+
+// const user : Person = new User("Alice", 30);
+// user.greet();
+
+interface Backpack<T> {
+    item: T;
+  add(obj: T): void;
+  get(): T;
 }
 
-const user : Person = new User("Alice", 30);
-user.greet();
+const backpack: Backpack<string> = {
+  item: "",
+  add(str) {
+    this.item = str;
+  },
+  get() {
+    return this.item;
+  }
+};
+
+backpack.add("saksham");
+console.log(backpack.get());
