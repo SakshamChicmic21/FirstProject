12e5b9c650f274c0aa56ac21cc723184205294bd
SAKSHAMGARG21 <sakshamgarggarg83@gmail.com>
2025-12-04 10:25:38 +0530

    done

---
+++ JavaScript-learning/therory.js
@@ -0,0 +1,4 @@
+// A closure is formed when a function remembers and accesses variables from its outer (parent) scope, even after the parent function has finished executing.
+
+// Definition (Interview-perfect):
+// 👉 A closure is the combination of a function + the lexical environment in which it was created.
